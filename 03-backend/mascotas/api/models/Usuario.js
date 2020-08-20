682f71f354730acea7232c1b88904d7b7d3bef73
xiriuxb <jorge.trujillo@epn.edu.ec>
2020-08-20 07:17:15 -0500

    clase 17-8-2020 y deber 4

--- 03-backend/mascotas/api/models/Usuario.js
+++ 03-backend/mascotas/api/models/Usuario.js
@@ -6,24 +6,38 @@
  */
 
 module.exports = {
-
+  //Diccionario de datosS
   attributes: {
-
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    cedula:{//nombre atributo
+      type: 'string',
+      required: true,//por defecto es falso
+      columnName: 'usr_cedula',
+      unique: true,
+      minLength:10,
+      maxLength:25
+    },
+    nombre:{
+      type: 'string',
+      minLength:3,
+      required: true//por defecto es falso
+    },
+    correo:{
+      type: 'string',
+      isEmail:true//por defecto es falso
+    },
+    estadoCivil:{
+      type: 'string',
+      isIn:['Soltero', 'Casado', 'Divorciado', 'Viudo','Union Libre'],
+      defaultsTo:'Soltero'
+    },
+    password:{
+      type: 'string',
+      regex:/^[a-zA-Z]\w{3,14}$/
+    },
+    pokemons:{//one to many
+      collection:'pokemon',//referencia al modelo
+      via:'usuario'//nombre foreign key
+    }
   },
-
 };
 
